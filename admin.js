ea0b00c4b1e9d4095a0080f5fc7090352dad5654
jaimemruiz <jmonruiz@go.ugr.es>
2026-08-01 19:18:47 +0300

    actualizacion

--- admin.js
+++ admin.js
@@ -1,20 +1,19 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@+esm'
+const SUPABASE_URL = 'https://iebufahobqzapgkatnoc.supabase.co/';
+const SUPABASE_KEY = 'sb_publishable_p89dhhXDyVBghroDrfK1qg_BPlxxid8'; 
 
-const supabaseUrl = 'TU_URL_DE_SUPABASE';
-const supabaseKey = 'TU_CLAVE_ANON'; // Aquí puedes usar la clave pública normal
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 const formLogin = document.getElementById('form-login');
 const seccionResultados = document.getElementById('seccion-resultados');
 const tablaDatos = document.getElementById('tabla-datos');
 
-// 1. Cuando metas tu correo y contraseña en el móvil y le des a entrar:
+// 1. Iniciar sesión
 formLogin.addEventListener('submit', async (e) => {
     e.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    const { data, error } = await supabase.auth.signInWithPassword({
+    const { data, error } = await supabaseClient.auth.signInWithPassword({
         email: email,
         password: password,
     });
@@ -22,23 +21,31 @@ formLogin.addEventListener('submit', async (e) => {
     if (error) {
         alert("Contraseña incorrecta o no eres tú.");
     } else {
-        // ¡Login correcto! Ocultamos el login y cargamos los datos
         formLogin.style.display = 'none';
         seccionResultados.style.display = 'block';
         cargarRespuestas();
     }
 });
 
-// 2. Función para descargar y pintar las respuestas
+// 2. Cargar respuestas
 async function cargarRespuestas() {
-    const { data, error } = await supabase
+    const { data, error } = await supabaseClient
         .from('respuestas')
         .select('*');
 
     if (error) {
         console.error("Error al cargar:", error);
     } else {
-        // Aquí dibujas los datos en tu tabla HTML del móvil
         console.log("Datos secretos:", data);
     }
 }
+
+// 3. Control del checkbox para ver la contraseña
+    const checkboxMostrar = document.getElementById('mostrar');
+    const inputPassword = document.getElementById('password');
+
+    if (checkboxMostrar && inputPassword) {
+        checkboxMostrar.addEventListener('change', function () {
+            inputPassword.type = this.checked ? 'text' : 'password';
+        });
+    }
