8648d9697127193dad23c071e2c8903745842c49
jaimemruiz <jmonruiz@go.ugr.es>
2026-08-01 19:48:55 +0300

    actualizacion

--- admin.js
+++ admin.js
@@ -27,25 +27,66 @@ formLogin.addEventListener('submit', async (e) => {
     }
 });
 
-// 2. Cargar respuestas
+/// 2. Cargar y pintar las respuestas de forma bonita
 async function cargarRespuestas() {
     const { data, error } = await supabaseClient
         .from('respuestas')
-        .select('*');
+        .select('*')
+        .eq('leido', false);
 
     if (error) {
         console.error("Error al cargar:", error);
     } else {
-        console.log("Datos secretos:", data);
+        const cuerpoTabla = document.getElementById('cuerpo-tabla');
+        cuerpoTabla.innerHTML = ''; // Limpiamos la tabla antes de pintar
+
+        data.forEach((fila) => {
+            const tr = document.createElement('tr');
+            
+            // Si quieres cambiar el diseño si ya está leído (puedes añadir una columna booleana 'leido' a tu tabla si lo deseas)
+            tr.innerHTML = `
+                <td class="celda">${fila.edad}</td>
+                <td class="celda">${fila.profesion}</td>
+                <td class="celda">${fila.dispositivo}</td>
+                <td class="celda">${fila.problema}</td>
+                <td class="celda">${fila.solucion_act}</td>
+                <td class="celda">
+                    <button class="btn-leido" onclick="leido(${fila.id})" id="${fila.id}" >
+                        Marcar leído
+                    </button>
+                </td>
+            `;
+            cuerpoTabla.appendChild(tr);
+        });
+
+        
     }
 }
 
-// 3. Control del checkbox para ver la contraseña
-    const checkboxMostrar = document.getElementById('mostrar');
-    const inputPassword = document.getElementById('password');
+async function leido(id) {
 
-    if (checkboxMostrar && inputPassword) {
-        checkboxMostrar.addEventListener('change', function () {
-            inputPassword.type = this.checked ? 'text' : 'password';
-        });
+    const {data, error} = await supabaseClient
+        .from('respuestas')
+        .update({ leido: true })
+        .eq('id', id);
+
+    if (error) {
+        console.error("Error al marcar como leído:", error);
+    } else {
+        document.getElementById(id).disabled = true;
+        document.getElementById(id).textContent = 'Leído ✓';
+        document.getElementById(id).style.backgroundColor = '#10b981';
     }
+
+    
+}
+
+// 3. Control del checkbox para ver la contraseña
+const checkboxMostrar = document.getElementById('mostrar');
+const inputPassword = document.getElementById('password');
+
+if (checkboxMostrar && inputPassword) {
+    checkboxMostrar.addEventListener('change', function () {
+        inputPassword.type = this.checked ? 'text' : 'password';
+    });
+}
